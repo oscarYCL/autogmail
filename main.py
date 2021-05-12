8dedc3d1be76af740751942586c0b7dc86a3dad5
cheuk lam yuen <190213447@stu.vtc.edu.hk>
2021-05-12 23:16:59 +0800

    add send email function

---
+++ main.py
@@ -0,0 +1,36 @@
+import smtplib
+from email.message import EmailMessage
+import password
+import csv
+
+client_name =[]
+to_email = []
+
+with open('client_list.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for line in reader:
+        to_email.append(line[2])
+        client_name.append(line[1])
+
+del to_email[0]
+del client_name[0]
+i = 0
+
+
+for email in to_email:
+    email_msg = EmailMessage()  
+    send_email = "awssns01@gmail.com"
+
+
+    email_msg['Subject'] = 'Subject Of The Email'  
+    email_msg['From'] = 'awssns'  
+    email_msg['To'] =  email
+    email_msg.set_content("Dear "+ client_name[i] + " :" + " Main Content or Body")  
+
+
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)  
+    server.login(send_email, password.password)  
+    server.send_message(email_msg)  
+    server.quit()
+    i += 1
+        
